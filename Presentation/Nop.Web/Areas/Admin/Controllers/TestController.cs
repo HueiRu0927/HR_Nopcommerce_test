c01226a4af0f89179942c5d130607a704056ef24
HueiRu <hueiru810927@stust.edu.tw>
2020-09-14 10:10:20 +0800

    nop

---
+++ Presentation/Nop.Web/Areas/Admin/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nop.Web.Areas.Admin.Controllers
+{
+    public class TestController : Controller
+    {
+
+        // Create / Edit / Delete
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
